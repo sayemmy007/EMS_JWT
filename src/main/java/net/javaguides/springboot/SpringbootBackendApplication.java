e5df7e8e22c0448ad7e7b2859c65fd7af1ff7179
sayemmy007 <sayemmy007@gmail.com>
2023-04-12 14:02:22 +0530

    sysoadded

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -11,6 +11,7 @@ public class SpringbootBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
+		System.out.println("Imran");
 	}
 	
 //	@Bean
